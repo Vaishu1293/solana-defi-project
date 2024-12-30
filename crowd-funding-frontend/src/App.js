d6f84362e9f83529065fc1fbd69243dd6851ec67
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2024-12-30 07:38:40 +0000

    solana-wallet-connect

--- crowd-funding-frontend/src/App.js
+++ crowd-funding-frontend/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  const checkIfWalletIsConnected = async() => {
+    try{
+      const { solana } = window;
+  
+      if(solana) {
+        if(solana.isPhantom) {
+          console.log("Phantom wallet found!");
+          const response = await solana.connect({
+            onlyIfTrusted: false
+          });
+          console.log("Connect wallet with publicKey", response.publicKey.toString());
+          setWalletAddress(response.publicKey.toString());
+        } else {
+          alert("Solana object not found! Get a phantom wallet");
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+    if(solana){
+      const response = await solana.connect();
+      console.log("Connected with publicKey", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const rendererNotConnectedContainer = () => {
+    <button onClick={connectWallet}>Connect to Wallet</button>
+  };
+
+  useEffect(() => {
+    const onLoad = async() => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  return <div className="App">{walletAddress && rendererNotConnectedContainer()}</div>
+};
 
 export default App;
